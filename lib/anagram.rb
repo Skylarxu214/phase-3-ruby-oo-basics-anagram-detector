9d1e8c39e6b7d9b3f90f564120eb019f769151a0
Tian Xu <xueqingxu0302@gmail.com>
2022-01-13 05:18:54 -0700

    commit tree 32dfc8a7527904003e6777bd6822e9d0ea0d9fa4

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :name
+    def initialize(value)
+        @name = value 
+    end
+    def match(array) 
+        array.filter{|word| word.split("").sort == @name.split("").sort }
+    end
+     
+end
